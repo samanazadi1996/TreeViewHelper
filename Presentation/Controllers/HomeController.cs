ff2568a7094919608c89b372e2deedc7306572c5
saman azadi <samanazadi1996@gmail.com>
2021-02-19 23:35:16 +0330

    Add project files.

---
+++ Presentation/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Presentation.Infrastructure;
+using Presentation.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var list1 = get();
+            ViewBag.List1 = list1;
+
+            var list2 = get();
+
+            return View(list2);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private List<TreeViewModel> get()
+        {
+            var list = new List<TreeViewModel>() {
+                new TreeViewModel() { Id = "1", Text = "one", ParentId =null },
+                new TreeViewModel() { Id = "2", Text = "two", ParentId =null },
+                new TreeViewModel() { Id = "3", Text = "three", ParentId =null },
+                new TreeViewModel() { Id = "4", Text = "four", ParentId ="1" },
+                new TreeViewModel() { Id = "5", Text = "five", ParentId ="1" },
+                new TreeViewModel() { Id = "6", Text = "six", ParentId ="1" },
+                new TreeViewModel() { Id = "7", Text = "seven", ParentId ="4" },
+                new TreeViewModel() { Id = "8", Text = "eight", ParentId ="5" },
+                new TreeViewModel() { Id = "9", Text = "nine", ParentId ="5" },
+                new TreeViewModel() { Id = "10", Text = "ten", ParentId ="4" },
+                new TreeViewModel() { Id = "11", Text = "eleven", ParentId ="7" },
+                new TreeViewModel() { Id = "12", Text = "twelve", ParentId ="11" },
+                new TreeViewModel() { Id = "13", Text = "thirteen", ParentId ="4" },
+                new TreeViewModel() { Id = "14", Text = "fourteen", ParentId ="2" },
+                new TreeViewModel() { Id = "15", Text = "fifteen", ParentId ="2" },
+                new TreeViewModel() { Id = "16", Text = "sixteen", ParentId ="2" }
+            };
+            return list;
+        }
+
+    }
+}
---
+++ TreeViewHelper/TreeViewHtmlHelper.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Routing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Encodings.Web;
+
+namespace TreeViewHtmlHelper
+{
+    public static class TreeViewHtmlHelper
+    {
+        public static IHtmlContent TreeView(this IHtmlHelper html, List<TreeViewModel> models)
+        {
+            return html.Create(models, null, new RouteValueDictionary(null));
+        }
+        public static IHtmlContent TreeView(this IHtmlHelper html, List<TreeViewModel> models, object treeViewAttributes)
+        {
+            return html.Create(models, null, new RouteValueDictionary(treeViewAttributes));
+        }
+        public static IHtmlContent TreeView(this IHtmlHelper html, List<TreeViewModel> models, object itemSelected, object treeViewAttributes)
+        {
+            return html.Create(models, itemSelected, new RouteValueDictionary(treeViewAttributes));
+        }
+        #region Private
+        private static IHtmlContent Create(this IHtmlHelper html, List<TreeViewModel> models, object itemSelected, IDictionary<string, object> htmlAttributes)
+        {
+            var select = new TagBuilder("select");
+            if (htmlAttributes != null)
+                foreach (var item in htmlAttributes)
+                    select.MergeAttribute(item.Key, item.Value.ToString());
+
+            select.MergeAttribute("dir", "rtl");
+            select.SetOption(models, null, "", itemSelected?.ToString());
+            return select;
+        }
+        private static void SetOption(this TagBuilder select, List<TreeViewModel> models, string parentId, string space, string itemSelected)
+        {
+            var m = models.Where(p => p.ParentId?.ToString() == parentId);
+            foreach (var item in m)
+            {
+                var option = new TagBuilder("option");
+                option.MergeAttribute("Value", item.Id.ToString());
+                option.MergeAttribute("ParentId", item.ParentId?.ToString());
+                if (item.Id.ToString() == itemSelected)
+                {
+                    option.MergeAttribute("Selected", "Selected");
+                }
+                option.InnerHtml.AppendHtml(space + item.Text);
+
+                select.InnerHtml.AppendHtml(option.ToHtmlString());
+
+                if (models.Any(p => p.ParentId == item.Id))
+                {
+                    select.SetOption(models, item.Id.ToString(), space + "&nbsp;&nbsp;&nbsp;&nbsp;", itemSelected);
+                }
+            }
+        }
+        private static string ToHtmlString(this TagBuilder tagBuilder)
+        {
+            var writer = new System.IO.StringWriter();
+            tagBuilder.WriteTo(writer, HtmlEncoder.Default);
+            return writer.ToString();
+        }
+        #endregion
+    }
+
+    #region Dto
+    public class TreeViewModel
+    {
+        public object Id { get; set; }
+        public string Text { get; set; }
+        public object ParentId { get; set; }
+    }
+    #endregion
+}
